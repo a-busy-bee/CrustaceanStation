51ed7d52fd8ac63631dd7fcb40d1fb39bded8a04
LizaTurovets <elizaveta@turovets.com>
2026-04-24 11:48:57 -0400

    perf bar not entirely integrated

--- Assets/Code/Scripts/Managers/PerformanceManager.cs
+++ Assets/Code/Scripts/Managers/PerformanceManager.cs
@@ -1,3 +1,4 @@
+using System.Numerics;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -6,12 +7,16 @@ public class PerformanceManager : MonoBehaviour
     private float barPercent;
     private int numSeen;
     private int numCorrect;
+    private bool isChanging;
+    private float currentVelocity; 
 
     [SerializeField] private Slider sliderKiosk;
-    [SerializeField] private Slider sliderSummary;
+    //[SerializeField] private Slider sliderSummary;
     [SerializeField] private Image sliderKioskColor;
-    [SerializeField] private Image sliderSummaryColor;
+    //[SerializeField] private Image sliderSummaryColor;
     [SerializeField] private Color[] sliderColors;
+
+    // (numSeen - numCorrect)/numCorrect
 
     private void Awake()
     {
@@ -22,33 +27,73 @@ public class PerformanceManager : MonoBehaviour
         }
         numSeen = PlayerPrefs.GetInt("PerformanceBarNumSeen");
         barPercent = PlayerPrefs.GetFloat("PerformanceBarPercent");
-        numCorrect = (int)(numSeen * barPercent);
+        numCorrect = (int)((float)numSeen / (float)(barPercent + 1));
     }
+
+    private void Start()
+    {
+        UpdateSlider();
+    }
+
+    [ContextMenu("Correct")]
     public void Correct()
+    {
+        
+        numSeen++;
+        Save();
+        UpdateSlider();
+    }
+
+    [ContextMenu("Incorrect")]
+    public void Incorrect()
     {
         numCorrect++;
         numSeen++;
-        Save();
-    }
 
-    public void Incorrect()
-    {
-        numSeen++;
         Save();
+        UpdateSlider();
     }
 
     private void Save()
     {
-        barPercent = (float)numCorrect / (float)numSeen;
+        barPercent = (float)(numSeen - numCorrect) / (float)numCorrect;
         PlayerPrefs.SetFloat("PerformanceBarPercent", barPercent);
         PlayerPrefs.SetInt("PerformanceBarNumSeen", numSeen);
     }
 
     private void UpdateSlider()
     {
-        sliderKiosk.value = barPercent;
-        sliderSummary.value = barPercent;
+        isChanging = true;
+        //sliderSummary.value = barPercent;
 
-        //)
+        // 0.7 for green/yellow boundary
+        // 0.2 for yellow/red boundary
+        if (barPercent >= 0.7f)
+        {
+            sliderKioskColor.color = sliderColors[2];
+        }
+        else if (barPercent >= 0.2)
+        {
+            sliderKioskColor.color = sliderColors[1];
+        }
+        else
+        {
+            sliderKioskColor.color = sliderColors[0];
+        }
     }
+
+	private void Update()
+	{
+        if (isChanging)
+        {
+            sliderKiosk.value = Mathf.SmoothDamp(sliderKiosk.value, barPercent, ref currentVelocity, 0.25f);
+
+            if (Mathf.Abs(barPercent - sliderKiosk.value) < 0.01f)
+            {
+                isChanging = false;
+            }
+
+        }
+	} 
+
 }
