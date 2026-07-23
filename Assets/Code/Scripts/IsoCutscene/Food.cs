cca1b46f5ff95b27d70cf5626835eda2a0f416af
lucy <lucindathart@gmail.com>
2026-07-22 21:25:27 -0400

    lift bottles

--- Assets/Code/Scripts/IsoCutscene/Food.cs
+++ Assets/Code/Scripts/IsoCutscene/Food.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
@@ -6,29 +7,48 @@ public class Food : MonoBehaviour, IPointerEnterHandler, IPointerClickHandler, I
     [SerializeField] private Animator hoverAnimator;
     [SerializeField] private GameObject particlesObject;
     [SerializeField] private Animator particlesObjectAnimator;
+    private Animator animator;
+    private bool active = false;
 
     private void Start()
     {
         hoverAnimator.enabled = false;
+        animator = GetComponent<Animator>();
     }
 
     public void OnPointerEnter(PointerEventData data)
     {
+        if (active) { return; }
         hoverAnimator.enabled = true;
         hoverAnimator.Play("OnHover");
     }
 
     public void OnPointerClick(PointerEventData data)
     {
+        if (active) { return; }
         hoverAnimator.enabled = true;
-        hoverAnimator.Play("StopHover");
+        //hoverAnimator.Play("StopHover");
+        StartCoroutine(LiftBottleAndParticles());
+    }
+
+    private IEnumerator LiftBottleAndParticles()
+    {
+        active = true;
+        animator.Play("Lift");
+        yield return new WaitForSeconds(1f);
+
         particlesObject.SetActive(true);
         particlesObjectAnimator.Play("Particles");
-        //Debug.Log("clicked");
+        yield return new WaitForSeconds(1f);
+
+        animator.Play("PutDown");
+        yield return new WaitForSeconds(0.2f);
+        active = false;
     }
 
     public void OnPointerExit(PointerEventData data)
     {
+        if (active) { return; }
         hoverAnimator.enabled = true;
         hoverAnimator.Play("StopHover");
     }
